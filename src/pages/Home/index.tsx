440befd1f12edd97b55ff1f90aa4cbe8d6f22a6f
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-07-05 14:58:55 -0300

    feat(Adding cards to homepage): :art:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import { Card } from '../../components/Card'
 import { Header } from '../../components/Header'
 import { useAuth } from '../../hooks/useAuth'
 
@@ -10,5 +12,27 @@ export function Home() {
   if (!state.isAuthenticated) {
     navigate('/login')
   }
-  return <Header />
+  return (
+    <>
+      <Header />
+      <div className="relative -mt-16 flex items-center justify-center gap-8">
+        <Card
+          title="Entradas"
+          value={35000}
+          icon={<ArrowCircleUp className="h-8 w-8 text-brand-green" />}
+        />
+        <Card
+          title="Saídas"
+          value={5000}
+          icon={<ArrowCircleDown className="h-8 w-8 text-brand-red" />}
+        />
+        <Card
+          title="Total"
+          value={30000}
+          icon={<CurrencyDollar className="h-8 w-8 text-brand-white" />}
+          isTotal
+        />
+      </div>
+    </>
+  )
 }
