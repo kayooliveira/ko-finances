b056c9e0aa850edd4336e68c7c98c70a95ab05f9
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-07-10 19:16:00 -0300

    refactor(Refactoring HomePage): :recycle:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,38 +1,30 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { Card } from '../../components/Card'
 import { Header } from '../../components/Header'
+import { NewTransactionModal } from '../../components/NewTransactionModal'
+import { Transactions } from '../../components/Transactions'
 import { useAuth } from '../../hooks/useAuth'
 
 export function Home() {
-  const { state } = useAuth()
+  const { state: auth } = useAuth()
   const navigate = useNavigate()
-  if (!state.isAuthenticated) {
+  if (!auth.isAuthenticated) {
     navigate('/login')
   }
   return (
     <>
       <Header />
-      <div className="relative -mt-16 flex items-center justify-center gap-8">
-        <Card
-          title="Entradas"
-          value={35000}
-          icon={<ArrowCircleUp className="h-8 w-8 text-brand-green" />}
-        />
-        <Card
-          title="Saídas"
-          value={5000}
-          icon={<ArrowCircleDown className="h-8 w-8 text-brand-red" />}
-        />
-        <Card
-          title="Total"
-          value={30000}
-          icon={<CurrencyDollar className="h-8 w-8 text-brand-white" />}
-          isTotal
-        />
-      </div>
+      <main className="mx-auto w-5/6 max-w-[1200px]">
+        <div className="relative mx-auto -mt-16 flex w-full items-center justify-between gap-8 overflow-x-scroll scrollbar-thin scrollbar-thumb-brand-red scrollbar-track-brand-background">
+          <Card type="incomes" />
+          <Card type="outcomes" />
+          <Card type="total" />
+        </div>
+        <NewTransactionModal />
+        <Transactions />
+      </main>
     </>
   )
 }
