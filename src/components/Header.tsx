129979d958fbf8e1e9d36f0285bdc20cbf2eacb9
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-07-10 19:16:35 -0300

    style(Updating header style and modal animations): :art:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import { Popover } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react'
 import { SignOut } from 'phosphor-react'
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 import { useAuth } from '../hooks/useAuth'
@@ -9,28 +9,37 @@ import { Logo } from './Logo'
 export function Header() {
   const { state, logout } = useAuth()
   return (
-    <header className="relative h-52 bg-brand-pink pt-8">
+    <header className="z-10 h-52 bg-brand-pink pt-8">
       <div className="mx-auto flex w-5/6 items-start justify-between">
         <Link to="/">
           <Logo />
         </Link>
-        <div>
-          <Popover className="flex w-20 flex-col items-center gap-2">
-            <Popover.Button
-              title={state.user.name}
-              className="group flex flex-col items-center justify-center"
-            >
-              <img
-                src={state.user.profileAvatar}
-                alt="Avatar do usuário"
-                referrerPolicy="no-referrer"
-                className="box-box w-14 rounded-full border-2 border-brand-green p-[2px] transition-all group-hover:p-1"
-              />
-              <span className="font-bold text-brand-white transition-colors group-hover:text-brand-green">
-                {state.user.name.split(' ')[0]}
-              </span>
-            </Popover.Button>
-            <Popover.Panel className="right-0 z-10 animate-slide-from-bottom">
+        <Popover className="flex w-20 flex-col items-center gap-2">
+          <Popover.Button
+            title={state.user.name}
+            className="group flex flex-col items-center justify-center"
+          >
+            <img
+              src={state.user.profileAvatar}
+              alt="Avatar do usuário"
+              referrerPolicy="no-referrer"
+              className="box-box w-14 rounded-full border-2 border-brand-green p-[2px] transition-all group-hover:p-1"
+            />
+            <span className="font-bold text-brand-white transition-colors group-hover:text-brand-green">
+              {state.user.name.split(' ')[0]}
+            </span>
+          </Popover.Button>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-3"
+            enterTo="opacity-100 translate-y-0"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0 translate-y-3"
+          >
+            <Popover.Panel className="right-0 z-10">
               <button
                 type="button"
                 title="sair"
@@ -40,8 +49,8 @@ export function Header() {
                 <SignOut weight="bold" /> Sair
               </button>
             </Popover.Panel>
-          </Popover>
-        </div>
+          </Transition.Child>
+        </Popover>
       </div>
     </header>
   )
